437e63a60c41946ec44538cd1826a44952e47197
Jason Mandozzi <jason.mandozzi@gmail.com>
2024-04-13 23:31:24 -0400

    Here I fixed the match geometry effect to handle the job card to job detail view transition better

--- JobHopper/Components/JobCard.swift
+++ JobHopper/Components/JobCard.swift
@@ -8,69 +8,72 @@
 import SwiftUI
 
 struct JobCard: View {
-    var cardNamespace: Namespace.ID
-    var detailNamespace: Namespace.ID
+    var namespace: Namespace.ID
     @State var job: JobOpening
     @Binding var showDetail: Bool
     let buttonAction: (() -> Void)
     
     var body: some View {
-        ZStack {
-            if showDetail {
+        if !showDetail {
+            ZStack {
                 RoundedRectangle(cornerRadius: 25.0)
-                    .matchedGeometryEffect(id: "detail background \(job.id)", in: detailNamespace)
-                    .transition(.scale(scale: 1))
                     .frame(maxWidth: .infinity)
                     .foregroundColor(.blue)
-                    .frame(height: 150)
-                
-            }
-            if !showDetail {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .matchedGeometryEffect(id: "detail background \(job.id)", in: detailNamespace)
-                        .transition(.scale(scale: 1))
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(.blue)
-                    VStack(alignment: .leading) {
-                        Group {
-                            Text(job.jobTitle)
-                                .matchedGeometryEffect(id: "jobTitle \(job.id)", in: cardNamespace)
-                                .transition(.scale(scale: 1))
-                                .font(.JHtitleMedium)
+                VStack(alignment: .leading) {
+                    Group {
+                        Text(job.jobTitle)
+                            .font(.JHtitleMedium)
+                            .truncationMode(.tail)
+                        Spacer()
+                        HStack {
+                            Text(job.company)
+                                .font(.JHtitleSmall)
                                 .truncationMode(.tail)
                             Spacer()
-                            HStack {
-                                Text(job.company)
-                                    .matchedGeometryEffect(id: "company \(job.id)", in: cardNamespace)
-                                    .transition(.scale(scale: 1))
-                                    .font(.JHtitleSmall)
-                                    .truncationMode(.tail)
-                                Spacer()
-                                Text(job.status.rawValue)
-                                    .matchedGeometryEffect(id: "jobStatus \(job.id)", in: cardNamespace)
-                                    .transition(.scale(scale: 1))
-                                    .font(.JHtitleSmall)
-                                    .truncationMode(.tail)
-                            }
+                            Text(job.status.rawValue)
+                                .font(.JHtitleSmall)
+                                .truncationMode(.tail)
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding()
                     }
                 }
-                .matchedGeometryEffect(id: "background \(job.id)", in: cardNamespace)
-                .transition(.scale(scale: 1))
-                .frame(height: 150)
-                .onTapGesture {
-                    buttonAction()
-                }
+                .padding()
             }
+            .matchedGeometryEffect(id: job.id, in: namespace)
+            .frame(height: 170)
+            .transition(.scale(scale: 1))
+            .onTapGesture {
+                buttonAction()
+            }
+        } else {
+            ZStack {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.blue)
+                VStack(alignment: .leading) {
+                    Group {
+                        Text(job.jobTitle)
+                            .font(.JHtitleMedium)
+                            .truncationMode(.tail)
+                        Spacer()
+                        HStack {
+                            Text(job.company)
+                                .font(.JHtitleSmall)
+                                .truncationMode(.tail)
+                            Spacer()
+                            Text(job.status.rawValue)
+                                .font(.JHtitleSmall)
+                                .truncationMode(.tail)
+                        }
+                    }
+                }
+                .padding()
+            }
+            .frame(height: 170)
         }
     }
 }
 
 #Preview(body: {
-    @Namespace var detailNamespace
-    @Namespace var cardNamespace
-    return JobCard(cardNamespace: cardNamespace, detailNamespace: detailNamespace, job: .init(jobTitle: "Title", company: "Company", status: .applied, interviewProcess: nil), showDetail: .constant(true), buttonAction: { print("Button Action") })
+    @Namespace var namespace
+    return JobCard(namespace: namespace, job: .init(jobTitle: "Title", company: "Company", status: .applied, interviewProcess: nil), showDetail: .constant(false), buttonAction: { print("Button Action") })
 })
--- JobHopper/ContentView.swift
+++ JobHopper/ContentView.swift
@@ -10,23 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var selectedTab = 2
     var body: some View {
-        TabView(selection: $selectedTab,
-                content:  {
-            ListView()
-                .tabItem {
-                    Image(systemName: "list.bullet.rectangle")
-            }.tag(1)
-            
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house")
-            }.tag(2)
-            
-            AccountView()
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-            }.tag(3)
-        })
+        ListView()
     }
 }
 
